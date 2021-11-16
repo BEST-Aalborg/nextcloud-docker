6263178fc1b96f6aa07a3af7661cbd4168d1f0eb
Dennis Vestergaard Værum <github@varum.dk>
2021-11-16 17:19:38 +0100

    make some change for nextcloud to handle bigger workload

--- post.d/maintain_db_scheme.sh
+++ post.d/maintain_db_scheme.sh
@@ -5,5 +5,11 @@ echo "### Maintain DB Schemes ###"
 
 docker-compose exec -T --user www-data app php occ db:add-missing-indices
 docker-compose exec -T --user www-data app php occ db:add-missing-columns
+docker-compose exec -T --user www-data app php occ db:add-missing-primary-keys
+docker-compose exec -T --user www-data app php occ db:convert-filecache-bigint --no-interaction
+docker-compose exec -T --user www-data app php occ db:convert-mysql-charset --no-interaction
+#docker-compose exec -T --user www-data app php occ db:convert-type --no-interaction
+docker-compose exec -T --user www-data app php occ maintenance:mimetype:update-db
+
 
 
