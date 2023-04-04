4cabb57bf94199c423e027136bb2a0598c0a98eb
Dennis Vestergaard Værum <github@varum.dk>
2023-04-05 00:27:52 +0200

    lot of clean-up after diff and merge-conflicts

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -11,7 +11,6 @@ fi
 TEST=${TEST:-}
 
 docker_tag() {
-<<<<<<< HEAD
     local image="$1"
     local filter="${2:-}"
     local url="https://registry.hub.docker.com/v2/repositories/${image}/tags"
@@ -65,12 +64,6 @@ get_next_major_release_number() {
       printf "%s", next_major_release
     }
     '
-=======
-    image="$1"
-    filter="$2"
-    curl --silent https://registry.hub.docker.com/v1/repositories/${image}/tags | \
-        jq -r ".[].name | select(. | test(\"${filter}\"))"
->>>>>>> 467baad (A lot of changes, but mostly clean-ups)
 }
 
 
@@ -78,16 +71,12 @@ for file in $(find . -name Dockerfile); do
     image_and_tag="$(sed -nE 's/FROM[[:space:]]+//p' "${file}")"
     tag="${image_and_tag##*:}"
     image="${image_and_tag%:$tag}"
-<<<<<<< HEAD
     envirment_file="config.env"
-=======
->>>>>>> 467baad (A lot of changes, but mostly clean-ups)
 
     ### Figure out when to switch/upgrade to the new version ###
     # Before version 18 it was possible to use the tag "production", but now that is only
     # available for paying customers. Nextcloud change how the tags works.
     # The benefit of the production tag, is that it doesn't update right away, when a new
-<<<<<<< HEAD
     # major version comes out, but instead would wait 2 intermediate releases.
     #
     # This if-statement was made to re-create the functionality there was
@@ -114,25 +103,11 @@ for file in $(find . -name Dockerfile); do
         _tmp_version="$(docker_tag "library/${image}" "^[0-9.]*-${_branch}$" | sed "s/-${_branch}//" | \
         get_next_major_release_number "$_version" 5)"
         _version="$(tail -n 1 <<<"$_tmp_version" | tr -d '\n')"
-=======
-    # major version comes out, but instead would wait 2 releases.
-    # This if statement in the script was created to re-create the functionality
-    # there was previously provided by the production tag.
-    if [ "${image}" == "nextcloud" ]; then
-        _branch=${tag##*-}
-        _version=${tag%-$_branch}
-        [ "${_version}" == production ] && _version=17
-
-        # Check if we are far enough into the next major release to that we want to upgrade
-        _check_new_version=$(expr ${_version} + 1)
-        [ -n "$(docker_tag "${image}" "^${_check_new_version}[0-9.]*5-${_branch}$")" ] && _version="${_check_new_version}"
->>>>>>> 467baad (A lot of changes, but mostly clean-ups)
 
         image_and_tag="${image}:${_version}-${_branch}"
 
         # Only pull new image if the envirement variable `TEST` is not set else `echo` the image
         if [ -z "${TEST}" ]; then
-<<<<<<< HEAD
             if grep --quiet 'NEXTCLOUD_VERSION' "${envirment_file}"; then
                 sed -i "s/NEXTCLOUD_VERSION=.*/NEXTCLOUD_VERSION=${_version}/" "${envirment_file}"
             else
@@ -173,11 +148,6 @@ for file in $(find . -name Dockerfile); do
             fi
         else
             echo "TEST: Set the new Elastic Search image to be '${image_and_tag}' in the file: ${envirment_file}"
-=======
-            sed -Ei "s/^FROM[[:space:]].*/FROM ${image_and_tag}/" "${file}"
-        else
-            echo "TEST: Set the new Nextcloud image to be '${image_and_tag}' in the file: ${file}"
->>>>>>> 467baad (A lot of changes, but mostly clean-ups)
         fi
     fi
 
@@ -190,9 +160,5 @@ for file in $(find . -name Dockerfile); do
 done
 
 # Only run if the envirement variable `TEST` is not set
-<<<<<<< HEAD
 [ -z "${TEST}" ] && docker-compose -f docker-compose.yml pull
-=======
-[ -z "${TEST}" ] && docker-compose -f ~/nextcloud/docker-compose.yml pull
->>>>>>> 467baad (A lot of changes, but mostly clean-ups)
 
--- post.d/maintain_db_scheme.sh
+++ post.d/maintain_db_scheme.sh
@@ -3,8 +3,6 @@
 
 echo "### Maintain DB Schemes ###"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 docker-compose exec -T --user www-data app php occ db:add-missing-indices
 docker-compose exec -T --user www-data app php occ db:add-missing-columns
 docker-compose exec -T --user www-data app php occ db:add-missing-primary-keys
@@ -13,17 +11,8 @@ docker-compose exec -T --user www-data app php occ db:convert-mysql-charset --no
 #docker-compose exec -T --user www-data app php occ db:convert-type --no-interaction
 docker-compose exec -T --user www-data app php occ maintenance:mimetype:update-db
 
-=======
 if [ -z "${DOCKER_COMPOSE_FILE:-}" ]; then
 	echo "ERROR: The variable  DOCKER_COMPOSE_FILE was not defined"
 	exit 0
 fi
-
-docker-compose -f "${DOCKER_COMPOSE_FILE}" exec -T --user www-data app php occ db:add-missing-indices
->>>>>>> 432a29c (Expost DOCKER_COMPOSE_FILE in start_and_watch.sh and added a post script for DB Schemes)
-=======
-docker-compose exec -T --user www-data app php occ db:add-missing-indices
-docker-compose exec -T --user www-data app php occ db:add-missing-columns
->>>>>>> 467baad (A lot of changes, but mostly clean-ups)
-
 
